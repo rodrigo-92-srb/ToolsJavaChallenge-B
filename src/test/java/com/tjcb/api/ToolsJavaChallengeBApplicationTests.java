16c2a8d77015cb334256f889619f7aeaa1b0ce30
Rodrigo Silva <rodrigo.92.srb@gmail.com>
2025-09-22 15:40:38 -0300

    Base Project

---
+++ src/test/java/com/tjcb/api/ToolsJavaChallengeBApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tjcb.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToolsJavaChallengeBApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
